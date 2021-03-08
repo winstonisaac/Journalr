3544a21b35f8c9b3019bbd55c9a73c18be5138cf
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-08 11:18:06 +0800

    added responsive landing page (with some placeholder images)

---
+++ app/javascript/packs/landing_page.js
@@ -0,0 +1,13 @@
+window.addEventListener("resize", changeImages);
+function changeImages(){
+  if(window.innerWidth >= 768){
+    document.getElementById('UI').setAttribute("src",'/uiDesktop.png')
+    document.querySelector('#disclaimer').innerHTML = 'Female 3D character from <a href="https://icons8.com/illustrations/illustration/casual-life-3d-likes" target="_blank">icons8.com</a></div>'
+  }
+  else{
+    document.getElementById('UI').setAttribute("src",'/ui.png')
+    document.querySelector('#disclaimer').innerHTML = 'Female 3D character from<br><a href="https://icons8.com/illustrations/illustration/casual-life-3d-likes" target="_blank">icons8.com</a></div>'
+  }
+
+}
+changeImages();
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users
-  root "categories#index"
   resources :categories do
     resources :tasks
   end
