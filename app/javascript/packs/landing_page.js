06265a64d8be688d1366e0c12d4aeee3478884ed
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-09 12:55:18 +0800

    finished mobile view of categories index, made assets/icons

--- app/javascript/packs/landing_page.js
+++ app/javascript/packs/landing_page.js
@@ -8,6 +8,12 @@ function changeImages(){
     document.getElementById('UI').setAttribute("src",'/ui.png')
     document.querySelector('#disclaimer').innerHTML = 'Female 3D character from<br><a href="https://icons8.com/illustrations/illustration/casual-life-3d-likes" target="_blank">icons8.com</a></div>'
   }
+  if(window.innerWidth < 360){
+    document.getElementById('logoIcon').setAttribute("src",'/svgs/logoIcon.svg')
+  }
+  if(window.innerWidth >= 360){
+    document.getElementById('logoIcon').setAttribute("src",'/svgs/logoType.svg')
+  }
 
 }
 changeImages();
