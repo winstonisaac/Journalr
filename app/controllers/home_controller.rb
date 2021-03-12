50662361b938093f2c4f05a7a803122e236951ab
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-12 15:58:29 +0800

    fixed some styles and added redirect from landing when logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :is_logged_in
   def index
+  end
+
+  private
+  
+  def is_logged_in
+    if current_user != nil
+      redirect_to categories_path
+    end
   end
 end
