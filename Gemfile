198280de1a6e96da3d4bc509cb9c172845973a8d
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-13 08:48:22 +0800

    initialization for testing / added gems etc.

--- Gemfile
+++ Gemfile
@@ -45,6 +45,10 @@ group :development do
 end
 
 group :test do
+  gem 'rspec-rails'
+  gem 'database_rewinder'
+  gem 'factory_bot_rails'
+  gem 'shoulda-matchers'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 3.26'
   gem 'selenium-webdriver'
---
+++ spec/rails_helper.rb
@@ -0,0 +1,38 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'rspec/rails'
+require 'shoulda/matchers'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+  config.include FactoryBot::Syntax::Methods
+  config.before(:suite) do
+    DatabaseRewinder.clean_all
+  end
+  config.after(:each) do
+    DatabaseRewinder.clean
+  end
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
