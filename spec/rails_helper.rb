de16acbf12e76050596c86135a47365ae93a8220
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-13 11:06:11 +0800

    added capybara to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'shoulda/matchers'
+require 'capybara/rspec'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
