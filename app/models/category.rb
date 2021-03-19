fb0c8d9f5466effdab914f56b404be1918394e9e
winstonisaac <marcwinstonisaac@gmail.com>
2021-03-19 16:39:36 +0800

    tasks will now be deleted when parent category is deleted

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   validates :name, presence:true
   validates_length_of :name, maximum: 18, allow_blank: false
 
-  has_many :tasks
+  has_many :tasks, :dependent => :delete_all
   belongs_to :user
 end
